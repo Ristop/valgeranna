f1bbff2f93d1d6e750ec2f353f66fdc74ebd56b5
Risto Pärnapuu <ristoparnapuu@gmail.com>
2016-03-01 11:02:09 +0200

    head js fix

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -9,18 +9,18 @@
 <link href='https://fonts.googleapis.com/css?family=Roboto:400' rel='stylesheet' type='text/css'>
 
 <!--javascript-->
-<script src={{URL::asset('/js/jquery.js')}}></script>
-<script src={{URL::asset('/js/mobile-header.js')}}></script>
-<script src={{URL::asset('/js/show-active-menu-item.js')}}></script>
-<script src={{URL::asset('/js/bootstrap.min.js')}}></script>
+<script src={{URL::asset('/js/jquery.js')}} defer></script>
+<script src={{URL::asset('/js/mobile-header.js')}} defer></script>
+<script src={{URL::asset('/js/show-active-menu-item.js')}} defer></script>
+<script src={{URL::asset('/js/bootstrap.min.js')}} defer></script>
 
 <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
 
-<script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js" defer></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js" defer></script>
 <![endif]-->
-<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script src={{URL::asset('/js/location-google-maps.js')}}></script>
+<script src="http://maps.google.com/maps/api/js?sensor=false" defer></script>
+<script src={{URL::asset('/js/location-google-maps.js')}} defer></script>

ad06385082d5cb6b8d10b9dea0c8b5b800cf6341
Helena <helena.talimaa@gmail.com>
2016-03-06 20:47:29 +0200

    added some comments

--- resources/views/pages/pictures.blade.php
+++ resources/views/pages/pictures.blade.php
@@ -4,17 +4,18 @@
     <div class="container">
         <div class="row">
             <?php
-            $dir = 'img/gallery';
-            $file_display = array('jpg', 'jpeg', 'png', 'gif');
+            $dir = 'img/gallery'; //pictures location
+            $file_display = array('jpg', 'jpeg', 'png', 'gif'); //what kind of pic formats we accept
             if (file_exists($dir) == false) :
-                echo 'Directory \'', $dir, '\' not found!';
+                echo 'Directory \'', $dir, '\' not found!'; //if directory doesn't exist, display error
             else :
-            $dir_contents = scandir($dir);
+            $dir_contents = scandir($dir); //get pics as an array
             foreach ($dir_contents as $file):
-            $explode = explode('.', $file);
-            $end = end($explode);
+            $explode = explode('.', $file); //explode breaks a string into an array
+            $end = end($explode); //end gives last element of an array
             $file_type = strtolower($end);
             if ($file !== '.' && $file !== '..' && in_array($file_type, $file_display) == true):?>
+<!--            Setting up thumbnail format:       -->
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href=<?php echo "$dir/$file"?>>
                     <img class="img-responsive" src=<?php echo "$dir/$file"?> alt=<?php echo"$file"?>>
@@ -24,8 +25,6 @@
             <?php endforeach; ?>
             <?php endif; ?>
         </div>
-
         <hr>
-
     </div>
 @stop

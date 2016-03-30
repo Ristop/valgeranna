12692c48f2c554aeb04191ac99a91247d3462081
Helena <helena.talimaa@gmail.com>
2016-03-30 21:26:01 +0300

    add donate button

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -13,8 +13,20 @@
                 <h3> <?php echo trans('contact.contact'); ?> </h3>
                 <p> <?php echo trans('contact.contactInfo'); ?> </p>
             </div>
+            <div class="col-sm-6">
+                <h3>Support us!</h3>
+                <p>Click Donate to donate us 1 euro.</p>
+                <button id = "payButton" type="submit" class="btn btn-default" value="Donate">Donate</button>
+            </div>
         </div>
     </div>
+
+    <script type="text/javascript">
+        document.getElementById("payButton").onclick = function () {
+            location.href = "";
+        };
+    </script>
+
     <script src="http://maps.google.com/maps/api/js?sensor=false" defer></script>
     <script src={{URL::asset('/js/location-google-maps.js')}} defer></script>
 

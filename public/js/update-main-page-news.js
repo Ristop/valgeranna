5fe49f9f45f9b1e6579f7943a44f3709d3428e31
Helena <helena.talimaa@gmail.com>
2016-04-09 23:14:34 +0300

    Showing new posts without refresh

--- public/js/update-main-page-news.js
+++ public/js/update-main-page-news.js
@@ -1,25 +1,39 @@
 (function poll() {
-    var posts = document.getElementById("news");
-    var firstPostID = posts.children[1].id;
-    var secondPostID = posts.children[2].id;
-    var firstPost = document.getElementsByName(firstPostID);
-    var secondPost = document.getElementById(secondPostID);
     setTimeout(function () {
         $.ajax({
             url: '/news/ajaxpoll',
             dataType: 'json',
             success: function (data) {
+                var posts = document.getElementById("news");
+                var firstPostID = posts.children[1].id;
+                var secondPostID = posts.children[2].id;
                 var ids = [];
+                var titles = [];
+                var contents = [];
+                var dates = [];
                 for (var key in data) {
                     ids.push(data[key].id);
+                    titles.push(data[key].title);
+                    contents.push(data[key].content);
+                    dates.push(data[key].created_at);
                 }
                 var databaseFirstID = ids[0];
                 var databaseSecondID = ids[1];
                 if ((databaseFirstID != firstPostID && databaseFirstID != secondPostID) ||
                     (databaseSecondID != firstPostID && databaseSecondID != secondPostID)) {
-                    alert('Uus uudis!');
-                    $(firstPostID).remove();
-                    location.reload();
+                    document.getElementById(firstPostID).setAttribute("id", databaseFirstID);
+                    document.getElementById(secondPostID).setAttribute("id", databaseSecondID);
+                    var first = document.getElementById(databaseSecondID);
+                    var second = document.getElementById(databaseFirstID);
+                    var mainPagePosts = [first, second];
+                    for (var i = 0; i < 2; i++) {
+                        var heading = mainPagePosts[i].children[0];
+                        var body = mainPagePosts[i].children[1];
+                        var footer = mainPagePosts[i].children[2];
+                        heading.innerHTML = "<h4>" + titles[i] + "</h4>";
+                        body.innerHTML = contents[i].substring(0, 200);
+                        footer.innerHTML = dates[i];
+                    }
                 }
             },
             complete: poll

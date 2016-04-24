8e079189998e1a0c1b48f8b24b1f3d941fadc8a5
Helena <helena.talimaa@gmail.com>
2016-04-24 21:25:42 +0300

    Add translation to rooms page

--- resources/views/pages/rooms.blade.php
+++ resources/views/pages/rooms.blade.php
@@ -10,21 +10,17 @@
                     </a>
                 </div>
                 <div class="col-xs-12 col-md-9">
-                    <h1>Kaheinimesetuba</h1>
+                    <h1>{{ trans('rooms.double') }}</h1>
                     <p>
-                        Kaheinimesetoa suurus on 12 ruutmeetrit. Toas on üks kaheinimesevoodi ning istumisnurk jne...
-                        <br>
-                        Mugavused: siseõu, rõdu, merevaade, grill
+                        {{ trans('rooms.doubleInfo') }}
                     </p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-md-9">
-                    <h1>Neljainimesetuba</h1>
+                    <h1>{{ trans('rooms.quadruple') }}</h1>
                     <p>
-                        Neljainimesetoa suurus on 18 ruutmeetrit. Toas kaks üheinimesevoodit ning üks kaheinimesevoodi jne...
-                        <br>
-                        Mugavused: siseõu, rõdu, merevaade, grill
+                        {{ trans('rooms.quadrupleInfo') }}
                     </p>
                 </div>
                 <div class="col-xs-12 col-md-3">
